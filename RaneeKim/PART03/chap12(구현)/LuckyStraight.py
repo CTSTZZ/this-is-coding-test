15f5dba34c24425bb360f2da81e0e9ea71f86c4b
Ranee <raneek0326@gmail.com>
2023-11-13 14:56:12 +0900

    lucky solved + time

---
+++ RaneeKim/PART03/chap12(구현)/LuckyStraight.py
@@ -0,0 +1,61 @@
+import sys
+import time
+
+input = sys.stdin.readline
+
+def input_N() :
+    N = input().strip() 
+    if 10 <= int(N) <= 99999999 and len(N)%2 == 0 : 
+        return int(N) 
+    return (input_N())
+
+def Lucky(S) :
+    list_n = [int(n) for n in str(S)]
+    count_l = count_r = 0
+    for i in range(len(list_n)//2) :
+        count_l += list_n[i]
+    for i in range(len(list_n)//2,len(list_n)) :
+        count_r += list_n[i]
+    if count_l == count_r :
+        return print("LUCKY") 
+    else : 
+        return print("READY")
+
+n = input_N()
+start = time.time()
+Lucky(n)
+end = time.time()
+print("time : ", end-start)
+
+'''
+>>> n = input_N()
+12345
+1234567898
+123402
+>>> start = time.time()
+>>> Lucky(n)
+LUCKY
+>>> end = time.time()
+>>> print("time : ", end-start)
+time :  0.0009856224060058594
+'''
+
+'''
+def input_N() :
+    N = input().strip() # N은 str형
+    if 10 <= int(N) <= 99999999 and len(N)%2 == 0 :  # 입력조건 안써줘도 맞긴맞음(백준확인완)
+        return int(N) # 정수를 반환한다고 했으니 int형으로
+    return (input_N())
+
+def Lucky(S) :
+    list_n = [int(n) for n in str(S)] # 자리수에 맞춰 하나씩 꺼내주기 위하여
+    count_l = count_r = 0
+    for i in range(len(list_n)//2) : # 왼쪽 반
+        count_l += list_n[i]
+    for i in range(len(list_n)//2,len(list_n)) : # 오른쪽 반
+        count_r += list_n[i]
+    if count_l == count_r : # 같으면 럭키
+        return print("LUCKY") # 백준에서 에러 났던 부분 : print를 안쓰고 return "LCUKY만 해줘서"
+    else : 
+        return print("READY") # 다르면 레뒤
+'''
