3dd4f86bc49f5c89abcf7527689fd2f0c05ffb3c
Ranee <raneek0326@gmail.com>
2023-11-18 16:55:53 +0900

    특정거리도시찾기ing

--- RaneeKim/PART03/Q14_WallCheck.py
+++ RaneeKim/PART03/Q14_WallCheck.py
@@ -15,13 +15,15 @@ def solution(n,weak,dist) :
     for start in range(length) : # (0 ~ 취약부분spot개수-1) 을 각각 시작점으로 설정 
         for friends in list(permutations(dist, len(dist))) : # 순열을 통해 친구(거리)를 나열할 수 있는 모든 조합을 생성
             # [ [3,5,7] , [3,7,5] , [5,3,7] , [5,7,3] , [7,3,5] , [7,5,3] ]
-            
+             # 1,3,4,9,10
             count = 1 # 투입할 친구의 수
             position = weak[start] + friends[count-1]
             # position = 취약부분spot + 투입된친구수-1 의 index에 해당하는 친구가갈수있는거리 -> 해당 친구가 확인할 수 있는 마지막 위치
                                         #-> 즉 정렬된 거리 list의 첫번째 거리부터 확인 , count는 1로 초기화되어있으므로 -1을 해야함
 
-            for index in range(start,start+length) : # 시작점 ~ 취약점의 모든 부분 확인
+            for index in range(start,start+length) : # 시작점 ~ 취약점의 모든 부분 
+                # start -> 0 1 2 3 4
+                # start + lengt -> 0+4, 1+4, 2+4, 4+4, 4+4
                 if position < weak[index] : # 확인을 하다가 위에서 정한 친구가 확인할 수 있는 마지막 위치보다 오버된 취약 spot이 있으면 
                     count += 1 # 친구한명 더 추가
                     if count > len(dist) : 
---
+++ RaneeKim/PART03/Q15_FindCity.py
@@ -0,0 +1,4 @@
+from collections import deque
+
+n,m,k,x = map(int,input().split())
+maps = []
