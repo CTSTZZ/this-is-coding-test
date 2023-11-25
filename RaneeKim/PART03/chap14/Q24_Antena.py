408fbebdc720a0375166f00205d86c698baf554e
Ranee <raneek0326@gmail.com>
2023-11-25 20:30:30 +0900

    안테나 시간초과

---
+++ RaneeKim/PART03/chap14/Q24_Antena.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+def result() :
+    n = int(input().strip())
+    antena = list(map(int, input().split()))
+
+    antena_sum = []
+    for i in antena :
+        c = 0
+        antena_dict = {}
+        for e in antena :
+           c += abs(i-e)
+        antena_dict['house_num'] = i
+        antena_dict['sum'] = c
+        antena_sum.append(antena_dict)
+    
+    antena_sum = sorted(antena_sum, key = lambda x : x['sum'])
+    print(antena_sum[0]['house_num'])
+
+    return
+
+
+result()
