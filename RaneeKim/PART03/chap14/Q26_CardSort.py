8aff5ba2c91701fb2501bc73bc9aef533fa56c61
Ranee <raneek0326@gmail.com>
2023-11-28 20:20:23 +0900

    예시만성공

---
+++ RaneeKim/PART03/chap14/Q26_CardSort.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def card_sort() :
+    n = int(input().strip())
+    card = []
+    for i in range(n) :
+        card.append(int(input().strip()))
+
+    card = sorted(card)
+    sum_s = []
+    n = len(card)
+
+    for _ in range(n) :
+        if len(card) >=2 :
+            card[1] = (card[0] + card[1])
+            sum_s.append(card[1])
+            del card[0]            
+    
+    return print(sum(sum_s))
+
+card_sort()
