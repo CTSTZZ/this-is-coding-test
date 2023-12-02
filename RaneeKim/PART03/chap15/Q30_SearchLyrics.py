7ba6effe19c4c209190e9e534e2d9f505dbbd2ce
Ranee <raneek0326@gmail.com>
2023-12-02 17:53:14 +0900

    효율성실패

--- RaneeKim/PART03/chap15/Q30_SearchLyrics.py
+++ RaneeKim/PART03/chap15/Q30_SearchLyrics.py
@@ -1,21 +1,34 @@
-words = ['frodo','front','frost','frozen','frame','kakao']
-queries = ['fro??','????o','fr???','fro???','pro?']
-queries_list = [[e for e in i] for i in queries]
-words_list = [[e for e in i] for i in words]
+import sys
+input = sys.stdin.readline
 
-answer = []
-for queryy in queries_list :
-    count = 0
-    for word in words_list :
-        if len(word) == len(queryy) :
-            cheak_index = []
-            for i in range(len(queryy)) :
-                if queryy[i] != '?' :
-                    cheak_index.append(i)
-            count_2 = 0
-            for c in cheak_index :
-                if word[c] == queryy[c] :
-                    count_2 += 1
-            if count_2 == len(cheak_index) :
-                count += 1
-    answer.append(count)
+def solution() :
+    words = list(input().split())
+    queries = list(input().split())
+    queries_list = [[e for e in i] for i in queries]
+    words_list = [[e for e in i] for i in words]
+
+    answer = []
+    for queryy in queries_list :
+        count = 0
+        for word in words_list :
+            if len(word) == len(queryy) :
+                cheak_index = []
+                for i in range(len(queryy)) :
+                    if queryy[i] != '?' :
+                        cheak_index.append(i)
+                count_2 = 0
+                for c in cheak_index :
+                    if word[c] == queryy[c] :
+                        count_2 += 1
+                if count_2 == len(cheak_index) :
+                    count += 1
+        answer.append(count)
+    
+    return answer
+
+solution()
+
+'''
+frodo front frost frozen frame kakao
+fro?? ????o fr??? fro??? pro?
+'''
