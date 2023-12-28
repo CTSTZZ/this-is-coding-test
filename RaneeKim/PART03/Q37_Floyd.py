43a3d16a929009402c77ba656b94ed5de1a4a3f1
Ranee <raneek0326@gmail.com>
2023-12-28 18:04:25 +0900

    플로이드공부

--- RaneeKim/PART03/Q37_Floyd.py
+++ RaneeKim/PART03/Q37_Floyd.py
@@ -2,6 +2,7 @@ import sys
 import heapq
 
 input = sys.stdin.readline
+
 n,m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 for _ in range(m) :
@@ -29,9 +30,43 @@ def solution(city) :
         if cost[i] == 100000 :
             cost[i] = 0
     
-    return print(cost[1:])
+    return cost[1:]
 
 for i in range(1,n+1) :
-    solution(i)
+    a = solution(i)
+    for e in a :
+        print(e, end = " ")
+    print()
 
 
+
+'''
+n = int(input().strip())
+m = int(input().strip())
+
+INF = int(1e9)
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for a in range(1,n+1) :
+    for b in range(1,n+1) :
+        if a == b :
+            graph[a][b] = 0
+
+for _ in range(m) :
+    a,b,cost = map(int, input().split())
+    if cost < graph[a][b] :
+        graph[a][b] = cost
+
+for i in range(1,n+1) :
+    for a in range(1,n+1) :
+        for b in range(1,n+1) :
+            graph[a][b] = min(graph[a][b], graph[a][i]+graph[i][b])
+
+for a in range(1,n+1) :
+    for b in range(1,n+1) :
+        if graph[a][b] == INF :
+            graph[a][b] = 0
+        print(graph[a][b], end=" ")
+    print()
+    
+'''
