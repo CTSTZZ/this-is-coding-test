43220027345fc710d728fe80839153721f2cadcc
Ranee <raneek0326@gmail.com>
2024-01-14 18:35:18 +0900

    커리큘럼ing

--- RaneeKim/PART02/chap10/Curriculum.py
+++ RaneeKim/PART02/chap10/Curriculum.py
@@ -2,15 +2,24 @@ import sys
 input = sys.stdin.readline
 
 n = int(input().strip())
-result = [[0]*(n+1) for _ in range(n+1)]
-curriculum = [[0]]
+curriculum = [[]] # 첫번째리스트 즉 curriculum[0] 은 비어있어야하므로 초기화를 이렇게 시킴
+time = [0] * (n+1)
+
 for i in range(n) :
     data = list(map(int,input().split()))
-    result[i+1][i+1] = data[0]
-    curriculum.append(data[1:-1])
+    time[i+1] = data[0]
+    curriculum.append(data[1:-1]) 
+    # [ [] , [] , [1] , [1] , [3,1] , [3] ]
 
 for i in range(len(curriculum)) :
-    if i == 0 :
+    if len(curriculum[i]) == 0 :
         continue
-    if curriculum[i] > 0 :
-        
+    else :
+        max_l = time[curriculum[i][0]]
+        for l in curriculum[i] :
+            if max_l <= time[l] :
+                max_l = time[l]
+        time[i] = time[i] + max_l
+
+for i in time[1:] :
+    print(i)
