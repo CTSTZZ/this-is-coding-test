9940ea0877d9814a97a4e8379f9b00bf6e7f9e5e
Ranee <raneek0326@gmail.com>
2024-01-19 21:59:37 +0900

    팀결성완

--- RaneeKim/PART02/chap10/MakeTeam.py
+++ RaneeKim/PART02/chap10/MakeTeam.py
@@ -1,3 +1,43 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent,x) :
+    if parent[x] != x :
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x] 
+
+'''
+parent 
+노드번호 0 1 2 3 4 5 6 
+부모노드   1 2 3 4 5 6
+'''
+
+def union(parent,a,b) :
+    a = find_parent(parent,a) 
+    b = find_parent(parent,b)
+    if a < b : # 부모노드 (a,b =parent[n]이므로)를 기준으로 숫자가 작은걸 기준으로 바뀜
+        parent[b] = a
+    else :
+        parent[a] = b
+
+n,m = map(int,input().split())
+
+parent = []
+for i in range(n+1) :
+    parent.append(i) # 부모노드를 자신의 노드번호로 초기화
+
+for _ in range(m) :
+    cheak, a,b = map(int,input().split())
+    if cheak == 0 :
+        union(parent,a,b)
+    elif cheak == 1 :
+        if find_parent(parent,a) == find_parent(parent,b) :
+            print("YES")
+        else : 
+            print("NO")
+
+'''
+1회차
 n,m = map(int, input().split())
 
 standard = [0] * (n+1)
@@ -20,3 +60,4 @@ for i in range(1,m+1) :
             print('YES')
         else :
             print('NO')
+'''
